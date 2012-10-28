538ec5c75683d2b877c85e206128fa210b8545f7
all4senses <all4senses@gmail.com>
2012-10-28 20:45:47 +0200

    Library block

--- modules/_Dmm/dmm_blocks/js/dmm_mainMenuBlocks.js
+++ modules/_Dmm/dmm_blocks/js/dmm_mainMenuBlocks.js
@@ -3,7 +3,7 @@
   Drupal.behaviors.dmm_mainMenuBlocks = {
     attach: function (context, settings) {
        
-       
+       $( "#tabs" ).tabs();
        console.log('blocks test!');
        
     }
