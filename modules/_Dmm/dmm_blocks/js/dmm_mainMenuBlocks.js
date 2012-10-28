b83e1f43ab0b7b0875a0b79c86d4ca89517f21d7
all4senses <all4senses@gmail.com>
2012-10-28 22:04:01 +0200

    Library block

--- modules/_Dmm/dmm_blocks/js/dmm_mainMenuBlocks.js
+++ modules/_Dmm/dmm_blocks/js/dmm_mainMenuBlocks.js
@@ -3,7 +3,12 @@
   Drupal.behaviors.dmm_mainMenuBlocks = {
     attach: function (context, settings) {
        
-       $( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+       $( "#tabs" )
+       .tabs({
+          event: "mouseover"
+        })
+       .addClass( "ui-tabs-vertical ui-helper-clearfix" );
+       
        $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
        
        console.log('blocks test!');
