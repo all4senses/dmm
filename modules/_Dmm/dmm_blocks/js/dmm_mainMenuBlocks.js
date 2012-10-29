db46760d916c3d36d2b5192bbba6cc0099c560d7
all4senses <all4senses@gmail.com>
2012-10-29 04:02:36 +0200

    Library block

--- modules/_Dmm/dmm_blocks/js/dmm_mainMenuBlocks.js
+++ modules/_Dmm/dmm_blocks/js/dmm_mainMenuBlocks.js
@@ -10,12 +10,14 @@
        .addClass( "ui-tabs-vertical ui-helper-clearfix" );
        
        $( ".tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
-       $( ".tabs li a" ).click(function(event){
-         if(event.preventDefault){
-            event.preventDefault();
-         }else{
-            event.returnValue = false; 
-         };
+       $( ".tabs li a" ).click(function(e){
+         e.preventDefault();
+         e.stopPropagation();
+//         if(e.preventDefault){
+//            e.preventDefault();
+//         }else{
+//            e.returnValue = false; 
+//         };
          return false; 
         });
        
