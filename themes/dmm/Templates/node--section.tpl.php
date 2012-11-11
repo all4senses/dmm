26fc22d12439e4b0fc94eb115287538c8499d189
all4senses <all4senses@gmail.com>
2012-11-11 02:20:34 +0200

    User content fix

--- themes/dmm/Templates/node--section.tpl.php
+++ themes/dmm/Templates/node--section.tpl.php
@@ -65,7 +65,7 @@
 <?php 
   if (isset($node->sectionBlocks)) {
     foreach ($node->sectionBlocks as $sectionBlock) {
-      echo $sectionBlock;
+      echo $sectionBlock . '<div class="bottom-clear"></div>';
     }
   }
 ?>
